70f8e6c89e524c0df4abf7d5d87cd0bc842667ad
Terri Jackson <89110920+terri731@users.noreply.github.com>
2021-10-09 14:15:00 -0500

    Add files via upload

---
+++ Resources/PyPoll.py
@@ -0,0 +1,10 @@
+# Open the election results and read the file
+file_to_load = "./election_results.csv"
+
+
+
+with open(file_to_load) as election_data:
+
+     # To do: perform analysis.
+     print(election_data)
+
